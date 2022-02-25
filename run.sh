246fd6038743c10abd59d9b23d5e201ef7393df5
Luis Díaz <ldiazn98@gmail.com>
2022-02-25 00:09:12 -0400

    added scripts to stop server; added some doc to scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+# Use this file to launch a development server
 docker-compose down -v
 docker-compose -f docker-compose.yml up -d --build
--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Use this file to start the production server
 docker-compose -f docker-compose.prod.yml down -v
 docker-compose -f docker-compose.prod.yml up -d --build
 docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
