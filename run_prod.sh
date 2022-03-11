8edfed983f4ad2a8ef7e28d3468b685e7e30008f
Luis Díaz <ldiazn98@gmail.com>
2022-03-11 00:34:05 -0400

    set up scrapyd service running in background

--- run_prod.sh
+++ run_prod.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Use this file to start the production server
+echo "Shutting down active servers..."
 docker-compose -f docker-compose.prod.yml down -v
+echo "Building docker composer file..."
 docker-compose -f docker-compose.prod.yml up -d --build
-docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
-docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
+echo "Collecting static files..."
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear &&\
+echo "Running migrations..."
+echo "Ready to go!"
+echo "to run migrations, use the following commnad:"
+echo "docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput"
+echo "You can test this server by requesting to localhost:1337/admin"
