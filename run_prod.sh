a8242f70f403aa997de21b669a551b2d95baf6bb
Luis Díaz <ldiazn98@gmail.com>
2022-03-23 23:04:15 -0400

    improved display of prints

--- run_prod.sh
+++ run_prod.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 # Use this file to start the production server
-echo "Shutting down active servers..."
-docker-compose -f docker-compose.prod.yml down -v
-echo "Building docker composer file..."
-docker-compose -f docker-compose.prod.yml up -d --build
-echo "Collecting static files..."
+
+# Colors
+COLOR_NC="$(tput sgr0)" # No Color
+COLOR_BLACK='\e[0;30m'
+COLOR_GRAY='\e[1;30m'
+COLOR_RED='\e[0;31m'
+COLOR_LIGHT_RED='\e[1;31m'
+COLOR_GREEN='$(tput setaf2)'
+COLOR_LIGHT_GREEN='\e[1;32m'
+COLOR_BROWN='\e[0;33m'
+COLOR_YELLOW='\e[1;33m'
+COLOR_BLUE='\e[0;34m'
+COLOR_LIGHT_BLUE='\e[1;34m'
+COLOR_PURPLE='\e[0;35m'
+COLOR_LIGHT_PURPLE='\e[1;35m'
+COLOR_CYAN='\e[0;36m'
+COLOR_LIGHT_CYAN='\e[1;36m'
+COLOR_LIGHT_GRAY='\e[0;37m'
+COLOR_WHITE='\e[1;37m'
+
+printf "${COLOR_LIGTH_BLUE}Shutting down active servers...${COLOR_NC}\n" &&\
+docker-compose -f docker-compose.prod.yml down -v &&\
+printf "${COLOR_LIGHT_BLUE}Building docker composer file...${COLOR_NC}\n" &&\
+docker-compose -f docker-compose.prod.yml up -d --build &&\
+printf "${COLOR_LIGHT_BLUE}Collecting static files...${COLOR_NC}\n" &&\
 docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear &&\
-echo "Running migrations..."
-echo "Ready to go!"
-echo "to run migrations, use the following commnad:"
-echo "docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput"
-echo "You can test this server by requesting to localhost:1337/admin"
+printf "${COLOR_LIGHT_GREEN}Ready to go!${COLOR_NC}\n" &&\
+printf "${COLOR_LIGHT_BLUE}to run migrations, use the following commnad:${COLOR_NC}\n" &&\
+printf "${COLOR_YELLOW}   docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput${COLOR_NC}\n" &&\
+printf "${COLOR_LIGHT_BLUE}You can test this server by requesting to ${COLOR_YELLOW}localhost:1337/admin${COLOR_NC}\n"
