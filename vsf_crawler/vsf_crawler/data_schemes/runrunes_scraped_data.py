bd76832a886c58a1334ce7a980dffecf7c8dcb74
Luis Díaz <ldiazn98@gmail.com>
2022-04-15 22:51:18 -0400

    added new runrunes scraper

---
+++ vsf_crawler/vsf_crawler/data_schemes/runrunes_scraped_data.py
@@ -0,0 +1,13 @@
+"""
+    Scraped data specific for runrunes
+"""
+
+# Local imports
+from vsf_crawler.data_schemes.base_scraped_data import BaseDataScheme
+
+class RunRunesScrapedData(BaseDataScheme):
+    """
+        Data scheme for el nacional
+    """
+    
+    
